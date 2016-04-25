ba09ec05f9b38e02c874997cb13bcc4b4d14e45f
Ted Shaffer <ted@brightsign.biz>
2016-04-24 19:48:44 -0700

    access electron TestModule.

--- scripts/App.js
+++ scripts/App.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
+import TestModule from '../../electronModules/testModuleOut';
 
 export default class App extends Component {
   render() {
+    let testModule = new TestModule();
+    testModule.invokeTest();
+
     return (
       // Add your component markup and other subcomponent references here.
       <h1>Hello, World!</h1>
