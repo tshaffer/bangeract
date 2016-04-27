ce1c25536383636377c2bb5e6d8a49ca3806419a
Ted Shaffer <ted@brightsign.biz>
2016-04-26 18:06:37 -0700

    working on making media library items draggable

--- scripts/components/medialibrary.js
+++ scripts/components/medialibrary.js
@@ -14,56 +14,34 @@ class Medialibrary extends Component {
         console.log("medialibrary::componentDidMount invoked");
     }
 
-// <ul className="flex-container wrap">
-// {photoNodes}
-// </ul>
+    mediaLibraryDragStartHandler(ev) {
 
-    pizza() {
-
-        let self = this;
-        let photoNodes = this.props.photoInfo.photos.map(function (photo) {
-            self.thumbUrl = "http://localhost:3000/photos/" + photo.thumbUrl.replace(" ", "%20");
-            self.photosById[photo.dbId] = photo;
-            return (
-                <li className="flex-item photoThumbsDiv" key={photo.dbId}>
-                    <img id={photo.dbId} src={self.thumbUrl} className="thumbImg" width={photo.width}
-                         height={photo.height} onClick={self.photoSelected.bind(self)}/>
-                </li>
-            );
-        });
+        console.log("dragStart");
+        // Add the target element's id to the data transfer object
+        // ev.dataTransfer.setData("text", ev.target.id);
+        ev.dataTransfer.setData("path", ev.target.dataset.path);
+        ev.dataTransfer.setData("name", ev.target.dataset.name);
+        ev.dataTransfer.dropEffect = "copy";
     }
 
-    // return (
-    //     <div className="photosDiv">
-    //         <ul className="flex-container wrap">
-    //             {photoNodes}
-    //         </ul>
-    //     </div>
-    // );
-
-    // <td>
-    //     <div class="mediaLibraryThumbDiv">
-    //         <img id="{{thumb.column0.id}}" data-name="{{thumb.column0.fileName}}" data-path="{{thumb.column0.path}}" ng-src="{{thumb.column0.thumbUrl}}" class="mediaLibraryThumbImg" draggable="true" ondragstart="mediaLibraryDragStartHandler(event);">
-    //         <p class="mediaLibraryThumbLbl">{{thumb.column0.fileName}}</p>
-    //     </div>
-    // </td>
-
-render () {
+    render () {
 
         let self = this;
-        let foo = this.props.thumbs;
+
         if (!this.props.thumbs || this.props.thumbs.length == 0) {
             return (
                 <div>Pizza</div>
             );
         }
 
+        // <img id={thumb.id} src={thumb.thumbUrl} className="mediaLibraryThumbImg" data-name={thumb.fileName} data-path={thumb.path} draggable="true" ondragstart={self.mediaLibraryDragStartHandler.bind(self)(event)}/>
+
         // also get thumbs from column1 or change the way it's built (the right answer)
         let mediaLibraryThumbs = this.props.thumbs.map(function (thumb) {
             console.log("look at my thumb");
             return (
                 <li className="flex-item mediaLibraryThumbDiv" key={thumb.id}>
-                    <img id={thumb.id} src={thumb.thumbUrl} className="mediaLibraryThumbImg"/>
+                    <img id={thumb.id} src={thumb.thumbUrl} className="mediaLibraryThumbImg" data-name={thumb.fileName} data-path={thumb.path} draggable="true" onDragStart={self.mediaLibraryDragStartHandler}/>
                     <p className="mediaLibraryThumbLbl">{thumb.fileName}</p>
                 </li>
             );
