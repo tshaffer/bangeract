6a70a4f12798dbc0ee1f038be47f06329d953861
Ted Shaffer <ted@brightsign.biz>
2016-05-03 12:58:19 -0700

    add some typings with limited success

--- scripts/index.tsx
+++ scripts/index.tsx
@@ -2,6 +2,9 @@ import React = require('react');
 import ReactDOM = require('react-dom');
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+// import Redux = require('redux');
+// import ReactRedux = require('react-redux');
+
 
 import App from './App';
 import reducers from './reducers';
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,5 +1,7 @@
 /// <reference path="react/react-dom.d.ts" />
 /// <reference path="react/react.d.ts" />
+/// <reference path="redux/redux.d.ts" />
+/// <reference path="react-redux/react-redux.d.ts" />
 /// <reference path="jquery/jquery.d.ts"/>
 
 require.config({
