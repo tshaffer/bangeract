960498236681aad94d2503cc6688ede17613d4cc
Ted Shaffer <ted@brightsign.biz>
2016-05-03 18:07:34 -0700

    small syntax change

--- scripts/index.tsx
+++ scripts/index.tsx
@@ -1,8 +1,7 @@
 import React = require('react');
 import ReactDOM = require('react-dom');
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-// import Redux = require('redux');
+import Redux = require('redux');
 // import ReactRedux = require('react-redux');
 
 
@@ -10,7 +9,7 @@ import App from './App';
 import reducers from './reducers';
 
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={Redux.createStore(reducers)}>
         <App />
     </Provider>
     , document.getElementById('root'));
